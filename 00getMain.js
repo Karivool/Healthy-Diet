c23199843808dd07067dc37d2fe5ac35a7e80db7
karivool <evelynlee34@gmail.com>
2019-02-19 17:51:26 -0500

    Continue working on extension

--- 00getMain.js
+++ 00getMain.js
@@ -3,11 +3,20 @@ let names = ['PROD', 'QA', 'Stage', 'Mario', 'Luigi', 'Bowser'];
 let pics = ['prod', 'qa', 'stage', 'mario', 'luigi', 'bowser'];
 
 function choices(servers, names, pics) {
-    let serverString = "";
-    for (var choice = 0; choice < servers.length; choice++) {
-      serverString += `<div class="server-choice"><img src="images/${pics[choice]}.jpg"/><div class="sc-text">${names[choice]}</div></div>`;
-    };
-    document.write(serverString);
+    chrome.runtime.sendMessage({getDibsPage: 0}, function(response){
+      let serverString = "";
+      let dibsURL = new URL (response.url);
+
+      let protocol = dibsURL.protocol + "//";
+      let pathname = dibsURL.pathname;
+
+      console.log(dibsURL);
+    });
 };
+
+// for (var choice = 0; choice < servers.length; choice++) {
+//   serverString += `<div class="server-choice"><img src="images/${pics[choice]}.jpg"/><div class="sc-text">${names[choice]}</div></div>`;
+// };
+// document.write(serverString);
 
 choices(servers, names, pics);
--- popup.js
+++ popup.js
@@ -1,7 +1,12 @@
-chrome.tabs.query(
-  {
-    'active': true, 'lastFocusedWindow': true}, function (tabs) {
-    var url = tabs[0].url;
-    var domain = url.hostname;
-    console.log(domain);
-  });
+chrome.runtime.onMessage.addListener(
+   function(request, sender, sendResponse) {
+      if (request){
+        console.log(request);
+        chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function(tabs){
+          console.log(tabs[0].url);
+          sendResponse({url: tabs[0].url});
+        });
+        return true;
+      }
+   }
+);
