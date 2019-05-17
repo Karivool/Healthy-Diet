97416474e20e78ea7d81b0ea04ad91627dab3b31
karivool <evelynlee34@gmail.com>
2019-05-17 15:37:43 -0400

    Attempt to set up URL retreival

--- public/app/background.js
+++ public/app/background.js
@@ -1,37 +1,84 @@
-chrome.runtime.onInstalled.addListener(function() {
-  chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
-    chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostContains: '1stdibs.com'},
-      })
-      ],
-          actions: [new chrome.declarativeContent.ShowPageAction()]
-    }]);
-  });
-});
+(function() {
+    const tabStorage = {};
+    const networkFilters = {
+        urls: [
+            "*://1stdibs.com/*",
+            "*://developer.mozilla.org/*"
+        ]
+    };
 
-let currentURL = function(tab){
-  let newUrlLand = new URL (tab.url);
-  console.log(newUrlLand);
-  return newUrlLand;
-};
-console.log("currentURL is:");
-console.log(currentURL());
+    chrome.webRequest.onBeforeRequest.addListener((details) => {
+        const { tabId, requestId, url, timeStamp } = details;
+        if (!tabStorage.hasOwnProperty(tabId)) {
+            return;
+        }
 
+        tabStorage[tabId].requests[requestId] = {
+            requestId: requestId,
+            url: url,
+            startTime: timeStamp,
+            status: 'pending'
+        };
+        console.log(tabStorage[tabId].requests[requestId]);
+    }, networkFilters);
 
-chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function (tabs){
-    currentURL(tabs[0]);
-});
+    chrome.webRequest.onCompleted.addListener((details) => {
+        const { tabId, requestId, timeStamp } = details;
+        if (!tabStorage.hasOwnProperty(tabId) || !tabStorage[tabId].requests.hasOwnProperty(requestId)) {
+            return;
+        }
 
-// chrome.runtime.onMessage.addListener(
-//    function(request, sender, sendResponse) {
-//       if (request.action == "getDibsPage"){
-//         console.log(request);
-//         chrome.tabs.query({'active': true, 'lastFocusedWindow': true}, function(tabs){
-//           console.log(tabs[0].url);
-//           sendResponse({url: tabs[0].url});
-//         });
-//         return true;
-//       }
-//    }
-// );
+        const request = tabStorage[tabId].requests[requestId];
+
+        Object.assign(request, {
+            endTime: timeStamp,
+            requestDuration: timeStamp - request.startTime,
+            status: 'complete'
+        });
+        console.log(tabStorage[tabId].requests[details.requestId]);
+    }, networkFilters);
+
+    chrome.webRequest.onErrorOccurred.addListener((details)=> {
+        const { tabId, requestId, timeStamp } = details;
+        if (!tabStorage.hasOwnProperty(tabId) || !tabStorage[tabId].requests.hasOwnProperty(requestId)) {
+            return;
+        }
+
+        const request = tabStorage[tabId].requests[requestId];
+        Object.assign(request, {
+            endTime: timeStamp,
+            status: 'error',
+        });
+        console.log(tabStorage[tabId].requests[requestId]);
+    }, networkFilters);
+
+    chrome.tabs.onActivated.addListener((tab) => {
+        const tabId = tab ? tab.tabId : chrome.tabs.TAB_ID_NONE;
+        if (!tabStorage.hasOwnProperty(tabId)) {
+            tabStorage[tabId] = {
+                id: tabId,
+                requests: {},
+                registerTime: new Date().getTime()
+            };
+        }
+    });
+
+    chrome.tabs.onRemoved.addListener((tab) => {
+        const tabId = tab.tabId;
+        if (!tabStorage.hasOwnProperty(tabId)) {
+            return;
+        }
+        tabStorage[tabId] = null;
+    });
+
+    chrome.runtime.onMessage.addListener((msg, sender, response) => {
+        switch (msg.type) {
+            case 'popupInit':
+                response(tabStorage[msg.tabId]);
+                break;
+            default:
+                response('unknown request');
+                break;
+        }
+    });
+}());
--- src/App.js
+++ src/App.js
@@ -1,14 +1,48 @@
+/*global chrome*/
 import React, { Component } from 'react';
 import Main from './Main.js';
+import UrlGetter from "./UrlGetter";
+import { getCurrentTab } from "./Utils";
 import './index.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      url: ""
+    };
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount");
+    getCurrentTab((tab) => {
+      chrome.runtime.sendMessage({type: 'popupInit', tabId: tab.id}, (tabs) => {
+      if (tabs) {
+        console.log("response found");
+        console.log(tabs);
+        this.setState({
+          url: Object.assign(this.state.url, tabs)
+        });
+      } else {
+        console.log("else");
+        console.log(tabs);
+      }
+      });
+    });
+  }
+
+  componentDidUpdate() {
+  }
+
   render() {
+    let currentUrl = this.state.url;
     return (
       <div className="App">
         <div class="title">
           <img src="images/title.png" alt=""/><div class="title-text">Healthy Diet - Server Switcher</div>
         </div>
+        <UrlGetter traffic={this.state.url}/>
+        Current URL: {currentUrl}
         <Main/>
       </div>
     );
