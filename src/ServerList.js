4e3f920a4b604255610de86acf1f7d6724e97515
Evelyn Lee <evelynlee34@gmail.com>
2019-05-18 22:36:38 -0400

    Edge-case excluded server links generated

--- src/ServerList.js
+++ src/ServerList.js
@@ -2,17 +2,77 @@ import React, { Component } from 'react';
 
 class ServerList extends Component {
 
+  makeLink(urlParts, serverName) {
+    if (urlParts === undefined) {
+      return "https://www.1stdibs.com/";
+    } else {
+      let newHost = "";
+      let hostParts = urlParts.host.split(".");
+
+      switch (hostParts.length) {
+        case 3:
+          // case 3 e.g.
+          // www.1stdibs.com || qa.1stdibs.com
+          if (hostParts[0] === "adminv2") {
+            newHost = `adminv2.${serverName}.1stdibs.com`;
+          } else if (hostParts[1] === "1stdibs") {
+            newHost = `${serverName}.1stdibs.com`;
+          } else {
+            newHost = urlParts.host;
+          }
+          break
+        case 4:
+          // case 4 e.g.
+          // guava.intranet.1stdibs.com || adminv2.stage.1stdibs.com
+          if (hostParts[0] === "adminv2") {
+            newHost = `adminv2.${serverName}.1stdibs.com`;
+          } else if (hostParts[1] === "intranet") {
+            newHost = `${serverName}.1stdibs.com`;
+          } else {
+            newHost = urlParts.host;
+          }
+          break
+        case 5:
+          // Always adminv2 with intranet
+          // e.g. adminv2.guava.intranet.1stdibs.com
+          if (hostParts[2] === "intranet") {
+            let checkProd = (serverName === "www") ? "" : `${serverName}.` ;
+            console.log(checkProd);
+            newHost = `adminv2.${checkProd}1stdibs.com`;
+          } else {
+            newHost = urlParts.host;
+          };
+          break
+        default:
+          newHost = urlParts.host;
+      }
+      return `${urlParts.protocol}//${newHost}${urlParts.pathname}${urlParts.search}`;
+    }
+  }
+
   render() {
     let servers = this.props.servers;
     let names = this.props.names;
     let pics = this.props.pics;
+    let urlParts = this.props.urlParts;
+    let linkMaker = this.makeLink;
 
     return(
       <div class="manual-selection">
         { servers.map(function(server, idx) {
             return (
               <div class="server-choice" key={idx} idx={idx}>
-                {<img src={`/images/${pics[idx]}.jpg`}/>}
+                {
+                  <a
+                    href={linkMaker(urlParts, servers[idx])}
+                    target="_blank"
+                    rel="noopener noreferrer">
+                    <img
+                      src={`/images/${pics[idx]}.jpg`}
+                      alt={names[idx]}
+                    />
+                  </a>
+                  }
                 <div class="sc-text">{names[idx]}</div>
               </div>
             );
